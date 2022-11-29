807c4bfd30a070b80a75545f807ea226eb55412e
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-11-29 17:20:44 +0000

    Added sample class

---
+++ src/test/kotlin/SampleTest.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class SampleTest {
+
+    private val testSample: Sample = Sample()
+
+    @Test
+    fun testSum() {
+        val expected = 42
+        assertEquals(expected, testSample.sum(40, 2))
+    }
+}
